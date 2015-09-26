aa2957b770e95a9f733a5123fffccf0755d51d4d
jnels <s0ckeyeus@yahoo.com>
2015-09-26 16:00:22 -0400

    Added free drawing to canvas

--- draw.js
+++ draw.js
@@ -2,7 +2,10 @@ var $canvasText;
 var canvas;
 var printText;
 var $userText="";
-
+var canvas = new fabric.Canvas('poetryCanvas', {
+    isDrawingMode: true
+		ctx.lineWidth: 15;
+  });
 
 //Allow user to add text to canvas
 $("#submit").click(function(){
@@ -11,12 +14,18 @@ $("#submit").click(function(){
   $("#outputText").append($userText);
 	//Add to canvas
 	$canvasText = $("#outputText").html();
-	canvas = new fabric.Canvas('poetryCanvas');
 	printText = new fabric.Text($canvasText, { left: 100, top: 100 });
 	canvas.add(printText);
 	//Hide form, show new button
 	$("#form").hide();
 	$("#addNew").css("display","block");
+	
+  isDrawingMode: true;
+
+
+	poetryCanvas.freeDrawingBrush.color = "purple";
+	poetryCanvas.freeDrawingBrush.width = 10;
+
 });
 
 //Clear fields to allow new text. Also clear canvas.
